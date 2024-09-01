a050b5d29e655e8cfa20fb90a9c11ea1d0cc673f
Ryan Dey <techrpd15113@gmail.com>
2024-09-01 15:39:13 -0500

    Add hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { ArrowRight, MoveRight } from "lucide-react";
+import { ArrowRight } from "lucide-react";
+import { ContactForm } from "./forms/contact-form";
 
 export default function Hero() {
   return (
@@ -10,38 +10,31 @@ export default function Hero() {
         {/* Left column */}
         <div className="flex flex-col justify-center space-y-10">
           <div className="space-y-6">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-              Transform your wood, transform your space
+            <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+              Castle Hills Stain & Restoration
             </h1>
-            <p className="max-w-[600px] text-zinc-500 md:text-xl dark:text-zinc-400">
-              Revive the natural beauty of your wood with a splash of color and
-              a touch of care. From faded decks to tired furniture, we bring out
-              the best in your wood and make your home shine. Let's make your
-              space the envy of Dallas-Ft. Worth!
+            <p className="max-w-[600px] text-zinc-600 md:text-xl">
+              Wood staining, fence installation, and property painting company
+              serving the Dallas-Ft. Worth area!
             </p>
           </div>
 
           {/* CTA buttons */}
           <div className="flex gap-2 min-[400px]:flex-row">
             <Button asChild>
-              <Link href="/contact">Get in Touch</Link>
+              <Link href="/contact">Get a free estimate</Link>
             </Button>
-            <Button variant={"ghost"} className="gap-1">
-              See our services <ArrowRight size={18} />
+            <Button variant="ghost" className="gap-1">
+              See what we offer <ArrowRight size={18} />
             </Button>
           </div>
         </div>
 
         {/* Right column */}
         <div className="flex items-center justify-center">
-          <Image
-            alt="Placeholder image"
-            className="aspect-[4/3] overflow-hidden rounded-xl object-cover object-center"
-            height="400"
-            src="/next.svg"
-            width="600"
-            priority
-          />
+          <article className="bg-white border border-zinc-200 p-6 rounded-xl shadow-lg">
+            <ContactForm />
+          </article>
         </div>
       </div>
     </section>

fc4ab8d8283f4366cd39cdcf0a8d1a6308b0d7cf
Ryan Dey <techrpd15113@gmail.com>
2024-09-02 11:00:12 -0500

    Update service page

---
+++ src/app/services/fence-staining/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FenceStaining() {
+  return <h1 className="text-xl font-bold">Fence Staining</h1>;
+}
--- src/app/services/service-one/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function ServiceOne() {
-  return <h1 className="text-xl font-bold">Service One</h1>
-}

154b00ca92b299e732d2eae0ab39d68bbed5dd1b
Ryan Dey <techrpd15113@gmail.com>
2024-09-02 10:59:48 -0500

    Navigation streamlining

--- src/components/nav/logo.tsx
+++ src/components/nav/logo.tsx
@@ -1,16 +1,8 @@
 import Link from "next/link";
 
-export interface LogoProps {
-  onStateChange: Function;
-}
-
-export default function Logo(props: LogoProps) {
+export default function Logo() {
   return (
-    <Link
-      href="/"
-      className="flex-shrink-0 flex items-center"
-      onClick={() => props.onStateChange(false)}
-    >
+    <Link href="/" className="flex-shrink-0 flex items-center">
       <img src="/ch-logo.svg" width={50} />
       <span className="ml-2 text-xl font-bold hidden md:block md:text-sm lg:text-lg">
         Castle Hills Stain & Restoration
--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -1,49 +1,51 @@
 "use client";
 
-import { useState } from "react";
+import {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  forwardRef,
+  useState,
+} from "react";
 import Link from "next/link";
-import { Menu, X, ChevronDown } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import NavBanner from "@/components/nav/nav-banner";
 import Logo from "@/components/nav/logo";
-import { DropdownNavLink, NavLink } from "./nav-link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMobileServicesOpen, setIsMobileServicesOpen] = useState(false);
 
   // Automatically populates desktop menus, remember to manually populate mobile menu at the bottom
-  const NAV_LINKS = [
-    {
-      name: "Home",
-      path: "/",
-      subpages: [],
-    },
-    {
-      name: "About",
-      path: "/#about",
-      subpages: [],
-    },
-    {
-      name: "Services",
-      path: "/services",
-      subpages: [
-        {
-          name: "Service 1",
-          path: "/services/service-one",
-        },
-        {
-          name: "Service 2",
-          path: "/services/service-two",
-        },
-      ],
-    },
+
+  const PAGES: { title: string; href: string }[] = [
+    { title: "Home", href: "/" },
+    { title: "About", href: "/#about" },
   ];
 
-  // Close mobile menu when a page is tapped
-  const handleMenuState = (value: boolean) => {
-    setIsMenuOpen(value);
-  };
+  const SERVICES: { title: string; href: string }[] = [
+    { title: "Fence Staining", href: "/fence-staining" },
+    { title: "Patio Cover Staining", href: "/patio-cover-staining" },
+    { title: "Pergola Staining", href: "/pergola-staining" },
+    { title: "Fence Installation", href: "/fence-installation" },
+    { title: "Patio Cover Installation", href: "/patio-cover-installation" },
+    { title: "Pergola Installation", href: "/pergola-installation" },
+    { title: "Interior Painting", href: "/interior-painting" },
+    { title: "Exterior Painting", href: "/exterior-painting" },
+  ];
 
   return (
     <nav className="sticky top-0 z-10 bg-background shadow-md">
@@ -55,30 +57,42 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           <div className="flex flex-row">
             {/* Logo and text */}
-            <Logo onStateChange={handleMenuState} />
+            <Logo />
 
             {/* Desktop navigation */}
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-                {/* Render regular or dropdown links */}
-                {NAV_LINKS.map((link) =>
-                  link.subpages.length > 1 ? (
-                    <DropdownNavLink
-                      name={link.name}
-                      subpages={link.subpages}
-                      key={link.name}
-                    />
-                  ) : (
-                    <NavLink
-                      name={link.name}
-                      path={link.path}
-                      key={link.name}
-                      onStateChange={handleMenuState}
-                    />
-                  )
-                )}
-              </div>
-            </div>
+            <NavigationMenu>
+              <NavigationMenuList className="hidden md:flex ml-5">
+                {/* Single pages */}
+                {PAGES.map((page) => (
+                  <NavigationMenuItem key={page.title}>
+                    <NavigationMenuLink asChild>
+                      <Link
+                        href={page.href}
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        {page.title}
+                      </Link>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                ))}
+
+                {/* Services dropdown */}
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Services</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                      {SERVICES.map((service) => (
+                        <ListItem
+                          key={service.title}
+                          title={service.title}
+                          href={`/services/${service.href}`}
+                        />
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
           </div>
 
           {/* Contact Me button */}
@@ -88,80 +102,86 @@ export default function Navbar() {
             </Button>
           </div>
 
-          {/* Mobile menu button */}
+          {/* Mobile Navigation */}
           <div className="md:hidden">
-            <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-zinc-700 hover:text-zinc-900 hover:bg-zinc-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-zinc-500 transition-all duration-200 ease-in-out"
-            >
-              <span className="sr-only">Open main menu</span>
-              {isMenuOpen ? (
-                <X
-                  className="block h-6 w-6 transition-transform duration-200 rotate-90"
-                  aria-hidden="true"
+            <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+              <SheetTrigger asChild>
+                <Button variant="ghost" size="icon" className="w-10 h-10 p-2">
+                  <Menu className="h-6 w-6" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="right" className="w-[300px] sm:w-[400px]">
+                <Image
+                  src="/ch-logo.svg"
+                  width={100}
+                  height={100}
+                  alt="Castle Hills Stain & Restoration Logo"
                 />
-              ) : (
-                <Menu
-                  className="block h-6 w-6 transition-transform duration-200"
-                  aria-hidden="true"
-                />
-              )}
-            </button>
-          </div>
-        </div>
-      </div>
+                <p className="text-xl font-bold mt-5">
+                  Castle Hills <br /> Stain & Restoration
+                </p>
+                <hr className="mt-6" />
 
-      {/* Mobile menu */}
-      <div
-        className={`md:hidden overflow-hidden transition-all duration-300 ease-in-out ${
-          isMenuOpen ? "max-h-96" : "max-h-0"
-        }`}
-      >
-        <div className="flex flex-col px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          {NAV_LINKS.map((link) =>
-            link.subpages.length === 0 ? (
-              <NavLink
-                key={link.name}
-                name={link.name}
-                path={link.path}
-                onStateChange={handleMenuState}
-              />
-            ) : null
-          )}
-          <div>
-            {/* Services */}
-            <button
-              onClick={() => setIsMobileServicesOpen(!isMobileServicesOpen)}
-              className="text-zinc-700 hover:bg-zinc-200 hover:text-zinc-900 px-3 py-2 rounded-md text-base font-medium transition-all duration-200 ease-in-out w-full text-left flex justify-between items-center"
-            >
-              Services
-              <ChevronDown
-                className={`h-4 w-4 transition-transform duration-200 ${
-                  isMobileServicesOpen ? "rotate-180" : ""
-                }`}
-              />
-            </button>
-            <div
-              className={`overflow-hidden transition-all duration-300 ease-in-out ${
-                isMobileServicesOpen ? "max-h-48" : "max-h-0"
-              }`}
-            >
-              <div className="pl-4 space-y-1">
-                <Link
-                  href="/services/service-one"
-                  onClick={() => setIsMenuOpen(false)}
-                  className="text-zinc-700 hover:bg-zinc-200 hover:text-zinc-900 block px-3 py-2 rounded-md text-sm font-medium transition-all duration-200 ease-in-out"
-                >
-                  Service One
-                </Link>
-              </div>
-            </div>
+                <nav className="flex flex-col gap-4 mt-6">
+                  <Link
+                    href="/"
+                    className="text-xl font-semibold"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    href="/#about"
+                    className="text-xl font-semibold"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    About
+                  </Link>
+                  <div className="flex flex-col gap-2">
+                    <h2 className="text-xl font-semibold">Services</h2>
+                    {SERVICES.map((service) => (
+                      <Link
+                        key={service.title}
+                        href={`/services/${service.href}`}
+                        className="ml-4 text-zinc-700 hover:underline"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        {service.title}
+                      </Link>
+                    ))}
+                  </div>
+                </nav>
+                <Button className="mt-[15%] w-full">
+                  Get your free estimate
+                </Button>
+              </SheetContent>
+            </Sheet>
           </div>
-          <Button className="w-full mt-4 transition-all duration-200 ease-in-out hover:scale-105">
-            Get my free estimate
-          </Button>
         </div>
       </div>
     </nav>
   );
 }
+
+const ListItem = forwardRef<ElementRef<"a">, ComponentPropsWithoutRef<"a">>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-zinc-100 hover:text-accent-foreground focus:bg-zinc-100 focus:text-accent-foreground",
+              className
+            )}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">{title}</div>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  }
+);
+ListItem.displayName = "ListItem";

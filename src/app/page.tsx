c4651b57913e6a637a96fca965745f31b1fc5d42
Ryan Dey <techrpd15113@gmail.com>
2024-09-02 11:44:44 -0500

    Clean up root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="flex h-64 bg-hero bg-contain bg-right rounded-xl mb-10"></div>
       <Hero />
-
       <About />
       <ServicesCards />
     </>

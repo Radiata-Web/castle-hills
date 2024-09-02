0f10f6389128637938a6dcc92c72cd501f4a9797
Ryan Dey <techrpd15113@gmail.com>
2024-09-02 11:42:54 -0500

    Add services pages

--- src/app/services/fence-staining/page.tsx
+++ src/app/services/fence-staining/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function FenceStaining() {
+export default function Page() {
   return <h1 className="text-xl font-bold">Fence Staining</h1>;
 }
---
+++ src/app/services/pergola-installation/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Page() {
+  return <h1 className="text-xl font-bold">Pergola Installation</h1>;
+}

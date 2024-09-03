9fad09919672886818d43d3d5e871fa5bbb7efa8
ryandey <techrpd15113@gmail.com>
2024-09-03 16:21:35 -0500

    Add footer to service page layout

--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -1,15 +1,21 @@
-import { Metadata } from "next";
+import Footer from "@/components/nav/footer"
+import { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "Castle Hills Stain & Restoration | Services",
   description:
     "Wood installation, wood restoration, wood stain, and property painting services in the Dallas-Ft. Worth area.",
-};
+}
 
 export default function ServicesLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-  return <section className="mt-12">{children}</section>;
+  return (
+    <>
+      <section className="mt-12">{children}</section>
+      <Footer />
+    </>
+  )
 }

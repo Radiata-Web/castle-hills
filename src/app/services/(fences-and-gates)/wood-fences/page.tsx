e7f23f1c3a5e719845cbf31e2fc690edcb2e5d0e
ryandey <techrpd15113@gmail.com>
2024-09-04 09:12:19 -0500

    Initial wood fence service page

--- src/app/services/(fences-and-gates)/wood-fences/page.tsx
+++ src/app/services/(fences-and-gates)/wood-fences/page.tsx
@@ -1,9 +1,46 @@
-import React from "react";
-
+import React from "react"
+import Image from "next/image"
 export default function Page() {
   return (
-    <h1 className="text-3xl font-bold sm:text-4xl xl:text-5xl/none">
-      Wood Fence Services
-    </h1>
-  );
+    <>
+      <header className="mb-12">
+        <h1 className="text-3xl font-bold sm:text-4xl xl:text-5xl/none">
+          Wood Fence Services
+        </h1>
+      </header>
+      <section className="grid gap-5 md:grid-cols-2 ">
+        <figure>
+          <Image
+            src="/fence.jpg"
+            alt="Wood Fence"
+            width={300}
+            height={500}
+            className="rounded-lg w-full md:max-w-[400px]"
+          />
+        </figure>
+        <article className="space-y-8">
+          <section>
+            <h2 className="text-2xl font-bold sm:text-3xl xl:text-4xl/none mb-4">
+              Wood Fence Installation
+            </h2>
+            <p>
+              We offer a wide range of wood fence installation services to meet
+              your needs. We can install a new fence, replace an existing fence,
+              or repair a damaged fence.
+            </p>
+          </section>
+          <section>
+            <h2 className="text-2xl font-bold sm:text-3xl xl:text-4xl/none mb-4">
+              Wood Fence Staining
+            </h2>
+            <p>
+              We offer a wide range of wood fence installation services to meet
+              your needs. We can install a new fence, replace an existing fence,
+              or repair a damaged fence.
+            </p>
+          </section>
+        </article>
+      </section>
+    </>
+  )
 }

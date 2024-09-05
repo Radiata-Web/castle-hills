24a5d28b99eb037a4209e7dd7cdf3e9b730e474b
ryandey <techrpd15113@gmail.com>
2024-09-05 18:29:42 -0500

    Polish serviceDesc component

--- src/components/misc/serviceDesc.tsx
+++ src/components/misc/serviceDesc.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { Button } from "../ui/button"
 
 interface ServiceDescProps {
   imagePosition: "left" | "right"
@@ -11,25 +12,25 @@ interface ServiceDescProps {
 export default function ServiceDesc(props: ServiceDescProps) {
   return (
     <article
-      className={`flex flex-col md:flex-row items-center gap-4 ${
+      className={`flex flex-col md:flex-row items-center gap-4 md:gap-10 lg:gap-14 ${
         props.imagePosition === "right" ? "md:flex-row-reverse" : ""
       }`}
     >
-      <figure className="w-full md:w-1/2">
+      <figure className="max-h-[400px] w-full md:w-1/2 overflow-hidden items-center rounded-lg">
         <Image
           src={props.image}
           alt={props.imageAlt}
           width={500}
           height={500}
-          layout="responsive"
-          className="rounded-lg w-full object-cover md:max-w-[300px]"
+          className="min-w-full object-cover md:max-w-[300px] object-right"
         />
       </figure>
-      <div className="w-full md:w-1/2">
+      <div className="w-full md:w-1/2 space-y-4">
         <h2 className="text-2xl font-bold sm:text-3xl xl:text-4xl/none mb-4">
           {props.header}
         </h2>
         <p className="text-zinc-800">{props.content}</p>
+        <Button>Get an Estimate</Button>
       </div>
     </article>
   )

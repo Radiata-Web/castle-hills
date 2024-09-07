c8b97f3c896bc55af7ba690f82f72e9cb232ae96
ryandey <techrpd15113@gmail.com>
2024-09-06 22:21:32 -0400

    Add service description component for interior pages

--- src/components/misc/serviceDesc.tsx
+++ src/components/misc/serviceDesc.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { Button } from "../ui/button"
+import Link from "next/link"
 
 interface ServiceDescProps {
   imagePosition: "left" | "right"
@@ -16,22 +17,29 @@ export default function ServiceDesc(props: ServiceDescProps) {
         props.imagePosition === "right" ? "md:flex-row-reverse" : ""
       }`}
     >
-      <figure className="max-h-[400px] w-full md:w-1/2 overflow-hidden items-center rounded-lg">
+      {/* Service Image */}
+      <figure className="max-h-[350px] w-full md:w-1/2 overflow-hidden items-center rounded-lg">
         <Image
           src={props.image}
           alt={props.imageAlt}
           width={500}
           height={500}
-          className="min-w-full object-cover md:max-w-[300px] object-right"
+          className="aspect-square min-w-full object-cover md:max-w-[250px] object-right"
         />
       </figure>
-      <div className="w-full md:w-1/2 space-y-4">
-        <h2 className="text-2xl font-bold sm:text-3xl xl:text-4xl/none mb-4">
-          {props.header}
-        </h2>
-        <p className="text-zinc-800">{props.content}</p>
-        <Button>Get an Estimate</Button>
-      </div>
+
+      {/* Service Description */}
+      <section className="w-full md:w-1/2 space-y-8">
+        <span className="space-y-4">
+          <h2 className="text-2xl font-bold sm:text-3xl xl:text-4xl/none mb-4">
+            {props.header}
+          </h2>
+          <p className="text-zinc-600">{props.content}</p>
+        </span>
+        <Button size="lg" asChild>
+          <Link href="#contact">Get an Estimate</Link>
+        </Button>
+      </section>
     </article>
   )
 }

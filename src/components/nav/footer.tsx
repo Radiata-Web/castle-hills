80f60460d107685ceee86acbab40018485355edf
ryandey <techrpd15113@gmail.com>
2024-09-06 23:09:51 -0400

    Add service category separation to footer

--- src/components/nav/footer.tsx
+++ src/components/nav/footer.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link"
 import Socials from "@/components/ui/socials"
-import { ALL_SERVICES } from "@/lib/data"
+import {
+  OUTDOOR_LIVING_SERVICES,
+  FENCING_SERVICES,
+  PAINTING_SERVICES,
+} from "@/lib/data"
 
 export default function Footer() {
   return (
-    <footer className="py-12 flex flex-col max-w-8xl mx-auto">
+    <footer className="py-12 flex flex-col max-w-8xl mx-auto rounded-lg">
       <section className="w-full">
         {/* Two-column section */}
-        <div className="flex flex-col md:flex-row justify-between space-y-8 md:space-y-0 mb-8">
+        <div className="flex flex-col md:flex-row justify-between space-y-8 md:space-y-0 mb-8 gap-4 md:gap-48">
           {/* Left Column */}
-          <div className="flex flex-col space-y-4 md:w-1/2">
+          <div className="flex flex-col space-y-4 md:w-1/4">
             {/* Logo */}
             <span className="flex flex-col items-start gap-4">
               <span className="flex flex-row items-center gap-4">
                 <img src="/ch-logo.svg" width={84} />
-                <p className="text-xl font-bold leading-tight">
+                <p className="text-xl font-bold leading-none">
                   Castle Hills <br />
                   Stain & Restoration
                 </p>
@@ -31,8 +35,9 @@ export default function Footer() {
           </div>
 
           {/* Right Column */}
-          <div className="md:w-1/2">
-            <nav className="grid grid-cols-2 gap-4 sm:grid-cols-2">
+          <div className="md:w-3/4">
+            <nav className="grid grid-cols-2 gap-6 md:gap-8 lg:grid-cols-4">
+              {/* Company */}
               <div>
                 <h3 className="font-semibold mb-2">Company</h3>
                 <ul className="space-y-1">
@@ -62,10 +67,46 @@ export default function Footer() {
                   </li>
                 </ul>
               </div>
+
+              {/* Fencing */}
               <div>
-                <h3 className="font-semibold mb-2">Services</h3>
+                <h3 className="font-semibold mb-2">Fence & Gate</h3>
                 <ul className="space-y-1">
-                  {ALL_SERVICES.map((service) => (
+                  {FENCING_SERVICES.map((service) => (
+                    <li key={service.title}>
+                      <Link
+                        href={`/services/${service.href}`}
+                        className="text-sm text-muted-foreground hover:text-primary"
+                      >
+                        {service.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              {/* Outdoor Living */}
+              <div>
+                <h3 className="font-semibold mb-2">Outdoor Living</h3>
+                <ul className="space-y-1">
+                  {OUTDOOR_LIVING_SERVICES.map((service) => (
+                    <li key={service.title}>
+                      <Link
+                        href={`/services/${service.href}`}
+                        className="text-sm text-muted-foreground hover:text-primary"
+                      >
+                        {service.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              {/* Painting */}
+              <div>
+                <h3 className="font-semibold mb-2">Painting</h3>
+                <ul className="space-y-1">
+                  {PAINTING_SERVICES.map((service) => (
                     <li key={service.title}>
                       <Link
                         href={`/services/${service.href}`}

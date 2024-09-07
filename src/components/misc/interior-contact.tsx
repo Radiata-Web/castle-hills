0bc06536694513b36c7f209731b2c06c2843c819
ryandey <techrpd15113@gmail.com>
2024-09-06 22:21:48 -0400

    Add interior contact component

--- src/components/misc/interior-contact.tsx
+++ src/components/misc/interior-contact.tsx
@@ -1,20 +1,27 @@
 import { ContactForm } from "../forms/contact-form"
-
+import ContactNumber from "@/components/misc/contactNumber"
 export default function InteriorContact() {
   return (
-    <section className="p-8 rounded-lg grid gap-4 shadow-xl border border-zinc-200 lg:grid-cols-2 my-8">
-      <div>
-        <h2 className="text-2xl font-bold sm:text-3xl xl:text-4xl/none mb-4">
-          Request a Free Estimate
-        </h2>
-        <p className="text-zinc-800">
-          Castle Hills Stain & Restoration is dedicated to bringing your
-          property dreams to life!
-        </p>
-      </div>
-      <div>
-        <ContactForm showHeader={false} />
-      </div>
-    </section>
+    <>
+      <div className="absolute -translate-y-52" id="contact"></div>
+      <section className="grid p-8 rounded-lg shadow-xl border border-zinc-200 gap-4 md:gap-10 lg:gap-14 lg:grid-cols-2 mt-20">
+        <div>
+          <p className="text-2xl font-bold sm:text-3xl xl:text-4xl/none mb-4">
+            Get your free estimate today!
+          </p>
+          <p className="text-zinc-800">
+            Castle Hills Stain & Restoration is dedicated to bringing your
+            property dreams to life!
+          </p>
+          <span className="mt-8 text-sm">
+            Give us a call:
+            <ContactNumber iconSize={25} textSize="text-xl" className="mt-2" />
+          </span>
+        </div>
+        <div>
+          <ContactForm showHeader={false} />
+        </div>
+      </section>
+    </>
   )
 }

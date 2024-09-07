7694dcbfe8d66e4a255448b628ff6f2290504cff
ryandey <techrpd15113@gmail.com>
2024-09-06 22:21:11 -0400

    Move data to separate file

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,12 +1,12 @@
 export const PAGES: { title: string; href: string }[] = [
   { title: "Home", href: "/" },
   { title: "About", href: "/#about" },
-];
+]
 
 export const FENCING_SERVICES: {
-  title: string;
-  href: string;
-  image: string;
+  title: string
+  href: string
+  image: string
 }[] = [
   {
     title: "Wood Fences",
@@ -23,12 +23,17 @@ export const FENCING_SERVICES: {
     href: "fence-stain",
     image: "/fence.jpg",
   },
-];
+  {
+    title: "Automatic Gates",
+    href: "automatic-gates",
+    image: "/fence.jpg",
+  },
+]
 
 export const OUTDOOR_LIVING_SERVICES: {
-  title: string;
-  href: string;
-  image: string;
+  title: string
+  href: string
+  image: string
 }[] = [
   {
     title: "Arbors & Pergolas",
@@ -40,12 +45,12 @@ export const OUTDOOR_LIVING_SERVICES: {
     href: "patio-covers",
     image: "/fence.jpg",
   },
-];
+]
 
 export const PAINTING_SERVICES: {
-  title: string;
-  href: string;
-  image: string;
+  title: string
+  href: string
+  image: string
 }[] = [
   {
     title: "Interior Painting",
@@ -57,9 +62,53 @@ export const PAINTING_SERVICES: {
     href: "exterior-painting",
     image: "/fence.jpg",
   },
-];
+]
 
 export const ALL_SERVICES = FENCING_SERVICES.concat(
   OUTDOOR_LIVING_SERVICES,
   PAINTING_SERVICES
-);
+)
+
+export const REVIEWS: {
+  id: number
+  text: string
+  rating: number
+  name: string
+}[] = [
+  {
+    id: 1,
+    rating: 5,
+    name: "Broderick N.",
+    text: "Highly recommend their service! Was responsive and professional and able to work with my busy schedule. Aj had great communication with me the whole time and made me feel appreciated and not like a number. Was able to make my older fence look like it was brand new!",
+  },
+  {
+    id: 2,
+    rating: 5,
+    name: "Stephanie G.",
+    text: "Castle Hills Stain & Restoration is a company that goes above and beyond for the customers. The professionalism is superb and the owner of the company listens to the client needs. The owner, Alexander, prioritizes the client and tries to make the job as customized as possible. I felt heard and taken care of! Highly recommend Castle Hills Stain & Restoration.",
+  },
+  {
+    id: 3,
+    rating: 5,
+    name: "Bethany C.",
+    text: "AJ went over and beyond to find the perfect stain for our new patio cover. He was very communicative and provided several stain samples and examples and made sure we felt comfortable with our choice. The project was started and completed promptly and we love how it turned out!",
+  },
+  {
+    id: 4,
+    rating: 5,
+    name: "Alvah S.",
+    text: "They just stained my fence and it looks absolutely amazing!!!!  The quality of stain was top tier and brought so much life back to my fence. What I appreciated the most was their attention to detail and care they took in my backyard. I had just remodeled my pool and decking so I was a little nervous about stain getting where it shouldn’t but they took all the necessary precautions. I would HIGHLY recommend Castle Hills Stain & Restoration!",
+  },
+  {
+    id: 5,
+    rating: 5,
+    name: "Ryan S.",
+    text: "Castle Hills Stain & Restoration did a great job repairing, power washing, and staining my fence. Quick and professional communication from start to finish. I highly recommend them to anyone looking to replace, repair, or stain their fences.",
+  },
+  {
+    id: 6,
+    rating: 5,
+    name: "Ian F.",
+    text: "AJ with Castle Hills Stain & Restoration has always executed our projects together at a high level. He has power washed and stained over 20,000 Square feet of fencing in the last few years for me. I have always received incredible compliments for our clients on the results of his service. I would highly recommend and encourage anyone in need of fence washing and  stain/paint to inquire about their services.",
+  },
+]

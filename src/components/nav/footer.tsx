da66c28bfea8108467a3492cc2d253181ef0ac59
ryandey <techrpd15113@gmail.com>
2024-09-07 11:32:12 -0400

    Adjust footer spacing

--- src/components/nav/footer.tsx
+++ src/components/nav/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
                 </p>
               </span>
 
-              <span>
+              <span className="min-w-full">
                 <p className="text-sm">
                   Fence and gate contractor serving the Dallas-Fort Worth
                   Metroplex

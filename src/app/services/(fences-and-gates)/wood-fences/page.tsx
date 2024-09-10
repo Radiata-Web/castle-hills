b4d4b6548090b0bb2aef8dd72f67112625fd4c72
ryandey <techrpd15113@gmail.com>
2024-09-10 16:07:10 -0500

    Update wooden fence images/copy

--- src/app/services/(fences-and-gates)/wood-fences/page.tsx
+++ src/app/services/(fences-and-gates)/wood-fences/page.tsx
@@ -22,15 +22,15 @@ export default function Page() {
       <section className="space-y-12 md:space-y-20">
         <ServiceDesc
           imagePosition="left"
-          image="/fence.jpg"
-          imageAlt="Wood Fence Installation"
+          image="fences/wood/fence-3"
+          imageAlt="Unstained wooden fence in a residential backyard from the sidewalk"
           header="Wood Fence Installation"
           content="Looking to enhance your property’s curb appeal and privacy? A beautifully installed wood fence can do just that, adding both charm and security to your home. Our wood fence installation service uses top-quality materials and skilled craftsmanship to create a durable, aesthetically pleasing barrier that stands up to the elements. Imagine your yard framed by a stunning wood fence that complements your landscape and boosts your home’s value."
         />
         <ServiceDesc
           imagePosition="right"
-          image="/fence.jpg"
-          imageAlt="Wood Fence Staining"
+          image="fences/wood/fence-4"
+          imageAlt="Brown stained wooden fence in a residential alleyway"
           header="Wood Fence Staining"
           content="Is your current fence looking weathered or worn? Our professional fence staining services are the perfect solution to breathe new life into your existing structure. Using top-quality stains and sealants, we can restore your fence’s original beauty while offering enhanced protection against the elements. We will carefully prepare and apply the stain, ensuring an even, long-lasting finish that revitalizes your fence's appearance and extends its lifespan."
         />

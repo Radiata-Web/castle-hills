98bd99b27e9e819bd65d82bb64ba37dec07c07f5
ryandey <techrpd15113@gmail.com>
2024-09-11 16:58:07 -0500

    Polish automatic gate page

--- src/app/services/(fences-and-gates)/automatic-gates/page.tsx
+++ src/app/services/(fences-and-gates)/automatic-gates/page.tsx
@@ -22,17 +22,17 @@ export default function Page() {
       <section className="space-y-12 md:space-y-20">
         <ServiceDesc
           imagePosition="left"
-          image="/fence.jpg"
-          imageAlt="Wood Fence Installation"
-          header="Transform Your Outdoor Space"
-          content="Automatic gate copy"
+          image="gates/gate-1"
+          imageAlt="Deep brown stained wooden automatic gate along a brown wooden fence on a driveway"
+          header="Automatic Wooden Gates"
+          content="Tired of manually opening your gate every time you come home? An automatic gate can provide both convenience and enhanced security for your property. Our automatic gate installation service offers seamless operation, durable materials, and customizable designs to fit your style. Imagine pulling up to your home, pressing a button, and watching your gate glide open effortlessly—adding both ease and sophistication to your daily routine."
         />
         <ServiceDesc
           imagePosition="right"
-          image="/fence.jpg"
-          imageAlt="Wood Fence Staining"
-          header="Enhance Your Outdoor Living"
-          content="Automatic gate copy"
+          image="gates/gate-2"
+          imageAlt="Black wrought iron automatic gate with elegant design attached to stone walls"
+          header="Automatic Metal Gates"
+          content="An automatic metal gate can offer both convenience and peace of mind. Our installation service provides sturdy, stylish gates with the latest automation technology for seamless operation. Picture yourself driving up to a gate that opens effortlessly with a touch, offering both protection and a sleek modern look that enhances your property’s value."
         />
       </section>
     </>

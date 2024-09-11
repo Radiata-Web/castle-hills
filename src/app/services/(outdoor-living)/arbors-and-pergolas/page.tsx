d77d9da175b7c39af45c522aa9918fb4e7619856
ryandey <techrpd15113@gmail.com>
2024-09-11 17:06:14 -0500

    Add arbor & pergola inst. and stain page

--- src/app/services/(outdoor-living)/arbors-and-pergolas/page.tsx
+++ src/app/services/(outdoor-living)/arbors-and-pergolas/page.tsx
@@ -22,17 +22,17 @@ export default function Page() {
       <section className="space-y-12 md:space-y-20">
         <ServiceDesc
           imagePosition="left"
-          image="/fence.jpg"
-          imageAlt="Wood Fence Installation"
-          header="Transform Your Outdoor Space"
-          content="A&P copy"
+          image="arbors/arbor-1"
+          imageAlt="Orange-brown arbor attached to house with green plants at its base, against a blue sky"
+          header="Arbor Installation & Staining"
+          content="Looking to add a beautiful focal point to your backyard? An expertly crafted arbors can instantly elevate your outdoor space, creating a charming, shaded area for relaxation or entertaining. Our arbor installation and staining services provide not only sturdy, elegant structures but also premium staining to enhance and protect the wood’s natural beauty."
         />
         <ServiceDesc
           imagePosition="right"
-          image="/fence.jpg"
-          imageAlt="Wood Fence Staining"
-          header="Enhance Your Outdoor Living"
-          content="A&P copy"
+          image="pergolas/pergola-6"
+          imageAlt="Dark brown stained wooden pergola in a home's backyard alcove, with lights strung across it and greenery beneath it"
+          header="Pergola Installation & Staining"
+          content="Dreaming of a stylish, shaded retreat in your backyard? A custom pergola can transform your outdoor space into the perfect spot for relaxation and entertaining. Our expert pergola installation and staining services deliver both beauty and durability, with high-quality craftsmanship and a rich stain that protects and enhances the wood’s natural charm."
         />
       </section>
     </>

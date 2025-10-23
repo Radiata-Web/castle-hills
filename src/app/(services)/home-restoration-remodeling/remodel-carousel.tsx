eab32f28bd8cc13a33efe67e6307c75c85a30538
Ryan Dey <ryandeyp@gmail.com>
2025-10-23 15:44:26 -0500

    Before/after carousel for remodels

---
+++ src/app/(services)/home-restoration-remodeling/remodel-carousel.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import React from "react";
+
+export default function remodelCarousel() {
+  return (
+    <Carousel
+      className="w-full max-w-[450px]"
+      plugins={[
+        Autoplay({
+          delay: 4000,
+        }),
+      ]}
+      opts={{
+        loop: true,
+      }}
+    >
+      <CarouselContent>
+        <CarouselItem>
+          <figure className="max-h-[768px] flex flex-col items-center gap-2">
+            <p className="text-xl font-bold">Before</p>
+            <Image
+              src={`/photos/${"restoration/restore-8"}.webp`}
+              alt="Beige painted corner wall with a stone fireplace, large and rounded tiled base, and large white accent piece on top"
+              width={1000}
+              height={1000}
+              className="md:max-w-[424px] bg-accent-light rounded-lg shadow-md"
+            />
+          </figure>
+        </CarouselItem>
+        <CarouselItem>
+          <figure className="max-h-[768px] flex flex-col items-center gap-2">
+            <p className="text-xl font-bold">After</p>
+            <Image
+              src={`/photos/${"restoration/restore-9"}.webp`}
+              alt="The same offwhite painted corner with a remodeled fireplace made of larger stones, a larger base of the same stone, and a cleaner, more squared design."
+              width={1000}
+              height={1000}
+              className="md:max-w-[424px] bg-accent-light rounded-lg shadow-md"
+            />
+          </figure>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
